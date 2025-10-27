0c605d3a727e746b172864136af527a07bcfd826
EthanHunt101 <ethandouglashunt@gmail.com>
2025-10-27 15:50:14 -0400

    Created a section for functions to be added

--- Main.py
+++ Main.py
@@ -2,6 +2,14 @@ from tkinter import *
 from tkinter import ttk
 import sv_ttk
 
+# All functions I create will be added up here
+
+
+
+
+
+
+
 window = Tk("Calculator")
 mainframe = ttk.Frame(window, padding = (3,3,12,12))
 mainframe.grid(column = 0, row = 0, sticky = (N, W, E, S))
