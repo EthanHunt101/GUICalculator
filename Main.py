762f129e3e2ff1746dc0871a2627035f5deb7524
EthanHunt101 <ethandouglashunt@gmail.com>
2025-10-27 15:46:39 -0400

    Renamed the buttons to be more descriptive

--- Main.py
+++ Main.py
@@ -8,64 +8,64 @@ mainframe.grid(column = 0, row = 0, sticky = (N, W, E, S))
 
 
 # Creates the 0 button and adds it to the window
-button = ttk.Button(window, width = 5, text = "0")
-button.grid(column = 1, row = 5, sticky = (W, E))
+button0 = ttk.Button(window, width = 5, text = "0")
+button0.grid(column = 1, row = 5, sticky = (W, E))
 
 # Creates the 1 button and adds it to the window
-button = ttk.Button(window, width = 5, text = "1")
-button.grid(column = 2, row = 5, sticky = (W, E))
+button1 = ttk.Button(window, width = 5, text = "1")
+button1.grid(column = 2, row = 5, sticky = (W, E))
 
 # Creates the 2 button and adds it to the window
-button = ttk.Button(window, width = 5, text = "2")
-button.grid(column = 3, row = 5, sticky = (W, E))
+button2 = ttk.Button(window, width = 5, text = "2")
+button2.grid(column = 3, row = 5, sticky = (W, E))
 
 # Creates the 3 button and adds it to the window
-button = ttk.Button(window, width = 5, text = "3")
-button.grid(column = 1, row = 4, sticky = (W, E))
+button3 = ttk.Button(window, width = 5, text = "3")
+button3.grid(column = 1, row = 4, sticky = (W, E))
 
 # Creates the 4 button and adds it to the window
-button = ttk.Button(window, width = 5, text = "4")
-button.grid(column = 2, row = 4, sticky = (W, E))
+button4 = ttk.Button(window, width = 5, text = "4")
+button4.grid(column = 2, row = 4, sticky = (W, E))
 
 # Creates the 5 button and adds it to the window
-button = ttk.Button(window, width = 5, text = "5")
-button.grid(column = 3, row = 4, sticky = (W, E))
+button5 = ttk.Button(window, width = 5, text = "5")
+button5.grid(column = 3, row = 4, sticky = (W, E))
 
 # Creates the 6 button and adds it to the window
-button = ttk.Button(window, width = 5, text = "6")
-button.grid(column = 1, row = 3, sticky = (W, E))
+button6 = ttk.Button(window, width = 5, text = "6")
+button6.grid(column = 1, row = 3, sticky = (W, E))
 
 # Creates the 7 button and adds it to the window
-button = ttk.Button(window, width = 5, text = "7")
-button.grid(column = 2, row = 3, sticky = (W, E))
+button7 = ttk.Button(window, width = 5, text = "7")
+button7.grid(column = 2, row = 3, sticky = (W, E))
 
 # Creates the 8 button and adds it to the window
-button = ttk.Button(window, width = 5, text = "8")
-button.grid(column = 3, row = 3, sticky = (W, E))
+button8 = ttk.Button(window, width = 5, text = "8")
+button8.grid(column = 3, row = 3, sticky = (W, E))
 
 # Creates the 9 button and adds it to the window
-button = ttk.Button(window, width = 5, text = "9")
-button.grid(column = 1, row = 2, sticky = (W, E))
+button9 = ttk.Button(window, width = 5, text = "9")
+button9.grid(column = 1, row = 2, sticky = (W, E))
 
 # Creates the buttons for the operands
-button = ttk.Button(window, width = 5, text = "/")
-button.grid(column = 4, row = 2, sticky = (W, E))
+button_div = ttk.Button(window, width = 5, text = "/")
+button_div.grid(column = 4, row = 2, sticky = (W, E))
 
-button = ttk.Button(window, width = 5, text = "x")
-button.grid(column = 4, row = 3, sticky = (W, E))
+button_mul = ttk.Button(window, width = 5, text = "x")
+button_mul.grid(column = 4, row = 3, sticky = (W, E))
 
-button = ttk.Button(window, width = 5, text = "-")
-button.grid(column = 4, row = 4, sticky = (W, E))
+button_sub = ttk.Button(window, width = 5, text = "-")
+button_sub.grid(column = 4, row = 4, sticky = (W, E))
 
-button = ttk.Button(window, width = 5, text = "+")
-button.grid(column = 4, row = 5, sticky = (W, E))
+button_add = ttk.Button(window, width = 5, text = "+")
+button_add.grid(column = 4, row = 5, sticky = (W, E))
 
 # Creates the clear and equals buttons
-button = ttk.Button(window, width = 5, text = "AC")
-button.grid(column = 2, row = 2, sticky = (W, E))
+button_clear = ttk.Button(window, width = 5, text = "AC")
+button_clear.grid(column = 2, row = 2, sticky = (W, E))
 
-button = ttk.Button(window, width = 5, text = "=")
-button.grid(column = 3, row = 2, sticky = (W, E))
+button_equal = ttk.Button(window, width = 5, text = "=")
+button_equal.grid(column = 3, row = 2, sticky = (W, E))
 # This is where the magic happens
 sv_ttk.set_theme("dark")
 
